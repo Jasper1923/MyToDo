03a3c2eede5ae1a084d03fb91d3060efb81db77e
jasper <jasper1923@hotmail.com>
2016-08-28 16:27:30 +0800

    usercontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'todos#index'
   resources :todos, except: [:show, :new]
   resources :users, only: [:new, :create]
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
