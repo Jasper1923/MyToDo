fedbe3c16407790b06482b3f6b4e43c79a9bb4ab
jasper <jasper1923@hotmail.com>
2016-08-28 16:58:30 +0800

    session finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def login_as(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      login_as user
+      flash[:notice] = "登录成功"
+      redirect_to todos_path
+    else
+      flash[:error]
+      render :new
+    end
   end
 
   def destroy
